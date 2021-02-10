3e4cac37b05fbb6c7f95ee04cae70ec49752418f
Leah Lee <leah@iterable.com>
2021-02-10 05:27:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -30,3 +30,17 @@ function doWhileLoop(int) {
   }
   while (incrementVariable() <= int);
 }
+
+// actual answer:
+//function doWhileLoop(num) {
+//	var i = 0;
+
+//	function incrementVariable() {
+//		i = i + 1;
+//    return i;
+//	}
+
+//  do {
+//    console.log("I run once regardless.");
+//  } while (incrementVariable() < num);
+//}
