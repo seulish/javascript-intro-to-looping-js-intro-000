1431886641fe7b91a7cd30c4b3d17bc1d4600186
Leah Lee <leah@iterable.com>
2021-02-10 05:16:48 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,15 +1,13 @@
-var myArray = [];
-
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      myArray.push("I am 1 strange loop.");
+      array.push("I am 1 strange loop.");
     }
     else {
-      myArray.push("I am ${i} strange loops.");
+      array.push("I am ${i} strange loops.");
     }
   }
-  return myArray;
+  return array;
 }
 
 
